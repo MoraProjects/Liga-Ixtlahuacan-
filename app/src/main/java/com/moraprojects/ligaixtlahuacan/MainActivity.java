0caf7b7a6f1b81824e82bb514c248508b9a1619a
MoraProjects <35740030+MoraProjects@users.noreply.github.com>
2018-10-17 00:33:41 -0500

    Cambio de planes
    Se elimino el menú anteriormente agregado

--- app/src/main/java/com/moraprojects/ligaixtlahuacan/MainActivity.java
+++ app/src/main/java/com/moraprojects/ligaixtlahuacan/MainActivity.java
@@ -5,12 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import com.hitomi.cmlibrary.CircleMenu;
-import com.hitomi.cmlibrary.OnMenuSelectedListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    String arrayNombre[]= {"Tabla","Calendario","Goleadores","Campos","Contacto"};
+
 
 
     @Override
@@ -18,18 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CircleMenu circleMenu = (CircleMenu)findViewById(R.id.circle_menu);
-        circleMenu.setMainMenu(Color.parseColor("#999B9B"),R.drawable.ic_menu,R.drawable.ic_close)
-                .addSubMenu(Color.parseColor("FFFFFF"),R.drawable.ic_tabla)
-                .addSubMenu(Color.parseColor("#9CA6AE"),R.drawable.ic_goleadores)
-                .addSubMenu(Color.parseColor("FFFFFF"),R.drawable.calendario)
-                .addSubMenu(Color.parseColor("FFFFFF"),R.drawable.ic_localizacion)
-                .addSubMenu(Color.parseColor("FFFFFF"),R.drawable.ic_contacto)
-                .setOnMenuSelectedListener(new OnMenuSelectedListener() {
-                    @Override
-                    public void onMenuSelected(int index) {
-                        Toast.makeText(MainActivity.this,"Elegiste " + arrayNombre[index], Toast.LENGTH_SHORT);
-                    }
-                });
+
     }
 }
